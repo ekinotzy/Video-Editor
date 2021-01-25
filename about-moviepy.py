d24b5d174cb06b946d286bc0db91af432b972401
ekinotzy <77970558+ekinotzy@users.noreply.github.com>
2021-01-25 10:04:51 -0300

    Create about-moviepy.py

---
+++ about-moviepy.py
@@ -0,0 +1,4 @@
+# MoviePy is a Python module for video editing, which can be used for basic operations (like cuts, concatenations, title insertions), video compositing
+# (a.k.a. non-linear editing), video processing, or to create advanced effects.
+
+# It can read and write the most common video formats, including GIF.
